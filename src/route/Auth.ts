220396da0d0f1a1a1c579497e91071048e768cb3
Karol <gilkarol304@gmail.com>
2022-03-14 23:08:21 +0100

    added celebrate validation

--- src/route/Auth.ts
+++ src/route/Auth.ts
@@ -1,5 +1,5 @@
+import { celebrate, Joi, Segments } from 'celebrate';
 import { Router } from 'express'
-import passport from 'passport';
 import { UserInterface } from '../model/User'
 import AuthService from '../service/AuthService';
 import UserService from '../service/AuthService'
@@ -7,7 +7,13 @@ import UserService from '../service/AuthService'
 const router = Router()
 
 // SIGNUP NEW USER
-router.post('/signup', async (req, res, next) => {
+router.post('/signup', celebrate({
+	body:{
+		nickname: Joi.string().min(3).required(),
+		email: Joi.string().email().required(),
+		password: Joi.string().min(5).required()
+	}
+	}), async (req, res, next) => {
 	try {
 		const { body } = req as { body: UserInterface }
 		await UserService.signup(body)
@@ -18,7 +24,12 @@ router.post('/signup', async (req, res, next) => {
 })
 
 // LOGIN USER
-router.post('/login', async (req, res, next) => {
+router.post('/login', celebrate({
+	body:{
+		nickname: Joi.string().min(3).required(),
+		password: Joi.string().min(5).required()
+	}
+	}), async (req, res, next) => {
 	try {
 		const { body } = req as { body: UserInterface }
 		const user = await AuthService.login(body)
